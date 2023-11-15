45b9cc8489ed7c9fc016ef7ad9278f9085ead491
Zairui Yang <zairuiy@upenn.edu>
2023-11-15 14:05:16 -0500

    initial backend setup

---
+++ backend/routes.js
@@ -0,0 +1,73 @@
+const mysql = require('mysql')
+const config = require('./config.json')
+
+// Creates MySQL connection using database credential provided in config.json
+// Do not edit. If the connection fails, make sure to check that config.json is filled out correctly
+const connection = mysql.createConnection({
+  host: config.rds_host,
+  user: config.rds_user,
+  password: config.rds_password,
+  port: config.rds_port,
+  database: config.rds_db
+});
+connection.connect((err) => err && console.log(err));
+
+/******************
+ * WARM UP ROUTES *
+ ******************/
+
+// Route 1: GET /author/:type
+const author = async function(req, res) {
+  // TODO (TASK 1): replace the values of name and pennKey with your own
+  const name = 'Zairui Yang';
+  const pennKey = 'zairuiy';
+
+  // checks the value of type the request parameters
+  // note that parameters are required and are specified in server.js in the endpoint by a colon (e.g. /author/:type)
+  if (req.params.type === 'name') {
+    // res.send returns data back to the requester via an HTTP response
+    res.send(`Created by ${name}`);
+  } else if (req.params.type === 'pennkey') {
+    // TODO (TASK 2): edit the else if condition to check if the request parameter is 'pennkey' and if so, send back response 'Created by [pennkey]'
+    res.send(`Created by ${pennKey}`);
+  } else {
+    // we can also send back an HTTP status code to indicate an improper request
+    res.status(400).send(`'${req.params.type}' is not a valid author type. Valid types are 'name' and 'pennkey'.`);
+  }
+}
+
+// Route 2: GET /random
+const random = async function(req, res) {
+    // you can use a ternary operator to check the value of request query values
+    // which can be particularly useful for setting the default value of queries
+    // note if users do not provide a value for the query it will be undefined, which is falsey
+    const explicit = req.query.explicit === 'true' ? 1 : 0;
+  
+    // Here is a complete example of how to query the database in JavaScript.
+    // Only a small change (unrelated to querying) is required for TASK 3 in this route.
+    connection.query(`
+      SELECT *
+      FROM Songs
+      WHERE explicit <= ${explicit}
+      ORDER BY RAND()
+      LIMIT 1
+    `, (err, data) => {
+      if (err || data.length === 0) {
+        // If there is an error for some reason, or if the query is empty (this should not be possible)
+        // print the error message and return an empty object instead
+        console.log(err);
+        // Be cognizant of the fact we return an empty object {}. For future routes, depending on the
+        // return type you may need to return an empty array [] instead.
+        res.json({});
+      } else {
+        // Here, we return results of the query as an object, keeping only relevant data
+        // being song_id and title which you will add. In this case, there is only one song
+        // so we just directly access the first element of the query results array (data)
+        // TODO (TASK 3): also return the song title in the response
+        res.json({
+          song_id: data[0].song_id,
+          title: data[0].title,
+        });
+      }
+    });
+  }
