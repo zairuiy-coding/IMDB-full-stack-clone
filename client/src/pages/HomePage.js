60062587e9040a9de9406dcb0c842b3063ed03da
Zairui Yang <zairuiy@upenn.edu>
2023-12-03 19:09:37 -0500

    Link each title in the homepage/toppage to productionInfoPage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -90,7 +90,9 @@ export default function HomePage({ type }) {
                                         align="center"
                                         key={d.primaryTitle + ":" + d.averageRating}
                                     >
-                                        {d.primaryTitle + ":" + d.averageRating}
+                                        <Link to={`/ProductionInfo/${d.titleId}`} style={{ textDecoration: 'none' }}>
+                                            {d.primaryTitle + ":" + d.averageRating}
+                                        </Link>
                                     </Typography>
                                 ))}
                             </ol>
